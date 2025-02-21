cdeaab39f0ecc8fbee7cd48ab4793b363b2b2588
Benedikt Kreis <kreis@cs.uni-bonn.de>
2025-02-21 09:41:02 +0100

    Added right depth

--- src/imitation/data/rollout.py
+++ src/imitation/data/rollout.py
@@ -148,8 +148,8 @@ class TrajectoryAccumulator:
             if self.remove_keys[0] == "heightmap_diff":
                 # obs = obs[:,:-3072]
                 first_part = obs[:, :13]
-                # last_part = obs[:, -(2*407040):]
-                last_part = obs[:, -407040:]
+                last_part = obs[:, -(2*407040):]
+                # last_part = obs[:, -407040:]
                 obs = np.concatenate((first_part, last_part), axis=1)
         trajs: List[types.TrajectoryWithRew] = []
         wrapped_obs = types.maybe_wrap_in_dictobs(obs)
@@ -561,8 +561,8 @@ def generate_trajectories(
         if remove_keys[0] == "heightmap_diff":
             # rec_obs = obs[:,:-3072]
             first_part = obs[:, :13]
-            # last_part = obs[:, -(2*407040):]
-            last_part = obs[:, -407040:]
+            last_part = obs[:, -(2*407040):]
+            # last_part = obs[:, -407040:]
             rec_obs = np.concatenate((first_part, last_part), axis=1)
     else:
         obs = venv.reset()
